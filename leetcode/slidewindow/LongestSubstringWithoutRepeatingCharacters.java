874ce52c183891cc7ccf9dc03c41e434c18eba34
ikaruga <ikaruga@voidsun.com>
2019-09-11 12:04:45 +0800

    https://leetcode.com/problems/longest-substring-without-repeating-characters/submissions/

---
+++ leetcode/slidewindow/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,26 @@
+package leetcode.slidewindow;
+
+/**
+ * @author ikaruga
+ * 2019/9/11-11:12 AM
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+
+    public int lengthOfLongestSubstring(String s) {
+        Integer[] charIndex = new Integer[256];
+        int maxLength = 0;
+        int left = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (charIndex[c] != null && charIndex[c] >= left) {
+                maxLength = Math.max(maxLength, i - left);
+                left = charIndex[c] + 1;
+            }else{
+                maxLength = Math.max(maxLength, i + 1 - left);
+            }
+            charIndex[c] = i;
+        }
+        return maxLength;
+    }
+
+}
