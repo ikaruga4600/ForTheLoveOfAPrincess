a71fa39c64e88ceca3294300e12e9cf5c4c90873
ikaruga <ikaruga@voidsun.com>
2019-10-11 21:38:08 +0800

    https://leetcode.com/problems/longest-palindromic-substring/ optmize

--- leetcode/LongestPalindromicSubstring.java
+++ leetcode/LongestPalindromicSubstring.java
@@ -12,49 +12,46 @@ public class LongestPalindromicSubstring {
         char[] charArr = s.toCharArray();
         int[] dp = new int[length];
         int[] dp2 = new int[length];
+        int oddIdx = 0, evenIdx = -1;
         for (int i = 0; i < length; i++) {
             dp[i] = 1;
             if (i < length - 1) {
                 if (charArr[i] == charArr[i + 1]) {
                     dp2[i] = 1;
+                    evenIdx = i;
                 }
             }
         }
-        int max = 1;
         int width = 1;
         while (true) {
             boolean nextLoop = false;
+            int currOddIdx = -1, currEvenIdx = -1;
             for (int i = width; i < length - width; i++) {
                 if (dp[i] == width && charArr[i - width] == charArr[i + width]) {
                     nextLoop = true;
                     dp[i]++;
+                    currOddIdx = i;
                 }
 
                 if (dp2[i] == width && i < length - width - 1 && charArr[i - width] == charArr[i + width + 1]) {
                     nextLoop = true;
                     dp2[i]++;
+                    currEvenIdx = i;
                 }
             }
             if (!nextLoop) {
-                int oddIdx = findIdx(dp, width);
-                int evenIdx = findIdx(dp2, width);
                 if (evenIdx != -1) {
                     return s.substring(evenIdx - width + 1, evenIdx + width + 1);
                 } else {
                     return s.substring(oddIdx - width + 1, oddIdx + width);
                 }
+            }else{
+                evenIdx = currEvenIdx;
+                oddIdx = currOddIdx;
             }
             width++;
         }
     }
 
-    private int findIdx(int[] arr, int val) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == val) {
-                return i;
-            }
-        }
-        return -1;
-    }
 
 }
